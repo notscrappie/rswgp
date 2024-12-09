43b7623432053bf803dcc3b2fa4b37eb6d9e7dc3
scrappie <notscrappie@proton.me>
2024-12-09 16:32:56 +0200

    refactor(comp/ui): fixes for the new API + linting
    - Also updated all UI imports, now they all come from @components and icons now come from lucide-react instead of react-icons.

--- src/components/ui/gameCard.tsx
+++ src/components/ui/gameCard.tsx
@@ -1,55 +1,56 @@
 'use client';
 
-import { ISteamGame } from "@/types";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./card";
-import { MdCategory, MdEventAvailable, MdPublish } from "react-icons/md";
-import { SiGamedeveloper } from "react-icons/si";
-import { ReactNode } from "react";
-import { Button } from "./button";
-import { useRouter } from "next/navigation";
+import { Button, Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@components';
+import { Code, Globe, List, Calendar } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import type { SteamGame } from '@/types';
+import { ReactNode } from 'react';
+import Link from 'next/link';
 
-export function GameCard({ game }: { game: ISteamGame }) {
-    const router = useRouter();
-    const appid = Number(Object.keys(game.fetchGameJson)[0]);
-    const gameData = game.fetchGameJson[appid].data;
+export function GameCard({ game }: { game: SteamGame }) {
+	const router = useRouter();
+	const appid = Number(Object.keys(game)[0]);
+	const gameData = game[appid].data;
 
-    return (
-        <Card className="bg-[#1a1a1a] text-white rounded-lg shadow-md overflow-hidden max-w-lg mx-auto ml-2 mr-2">
-            <CardHeader className="p-4">
-                <CardTitle className="font-sans font-semibold text-lg text-center">
-                    {gameData.name}
-                </CardTitle>
-                <CardDescription className="font-sans text-sm text-white/70 mt-1 text-left">
-                    {gameData.short_description}
-                </CardDescription>
-            </CardHeader>
-            <CardContent className="p-4 space-y-3">
-                <InfoRow icon={<SiGamedeveloper />} label="Developed by" content={gameData.developers.join(", ")} />
-                <InfoRow icon={<MdPublish />} label="Published by" content={gameData.publishers.join(", ")} />
-                <InfoRow icon={<MdCategory />} label="Genres" content={gameData.genres.map((genre) => genre.description).join(", ")} />
-                <InfoRow icon={<MdEventAvailable />} label="Release Date" content={gameData.release_date.date} />
-                <video
-                    src={gameData.movies[0]?.mp4?.max}
-                    controls
-                    className="w-full max-w-screen-sm rounded-lg mt-4 mx-auto"
-                />
-            </CardContent>
-            <CardFooter className="flex flex-col sm:flex-row">
-                <Button className="font-sans text-semibold text-black w-full mr-1" onClick={() => router.refresh()}>I'm feeling lucky today</Button>
-                <Button className="font-sans text-semibold text-black w-full ml-1" onClick={() => router.push(`https://store.steampowered.com/app/${gameData.steam_appid}`)}>Visit Steam Page</Button>
-            </CardFooter>
-        </Card>
-    );
+	return (
+		<Card className="bg-[#1a1a1a] text-white rounded-lg shadow-md overflow-hidden w-full max-w-3xl mx-auto p-4 md:p-6 mb-10">
+			<CardHeader className="p-2">
+				<CardTitle className="font-sans font-semibold text-lg md:text-xl text-left overflow-wrap">
+					{gameData.name}
+				</CardTitle>
+				<CardDescription className="font-sans text-sm text-white/70 mt-2 text-left overflow-wrap">
+					{gameData.short_description}
+				</CardDescription>
+			</CardHeader>
+			<CardContent className="p-2 space-y-3">
+				<InfoRow icon={<Code className='w-[18px] h-[18px]' />} label="Developed by" content={gameData.developers.join(', ')} />
+				<InfoRow icon={<Globe className='w-[18px] h-[18px]' />} label="Published by" content={gameData.publishers.join(', ')} />
+				<InfoRow icon={<List className='w-[18px] h-[18px]' />} label="Genres" content={gameData.genres.map((genre) => genre.description).join(', ')} />
+				<InfoRow icon={<Calendar className='w-[18px] h-[18px]' />} label="Release Date" content={gameData.release_date.date} />
+				<video
+					src={gameData.movies[0]?.mp4?.max || ''}
+					controls
+					className="w-full aspect-video rounded-lg"
+				/>
+			</CardContent>
+			<CardFooter className="font-sans font-semibold text-black flex flex-col sm:flex-row gap-4 mt-4 w-full">
+				<Button className="flex-1 w-full sm:w-auto text-center p-3" onClick={() => router.refresh()}>I'm feeling lucky again</Button>
+				<Link className="flex-1 w-full" href={`https://store.steampowered.com/app/${gameData.steam_appid}`}>
+					<Button className="w-full text-center p-3">Visit Steam Page</Button>
+				</Link>
+			</CardFooter>
+		</Card>
+	);
 }
 
 function InfoRow({ icon, label, content }: { icon: ReactNode; label: string; content: string }) {
-    return (
-        <div className="flex items-center text-sm space-x-2">
-            <span className="bg-white/20 p-2 rounded-lg shadow-md flex items-center">
-                <span className="text-white">{icon}</span>
-            </span>
-            <h1 className="font-semibold text-white">{label}:</h1>
-            <p className="ml-1 text-white/80">{content}</p>
-        </div>
-    );
+	return (
+		<div className="flex items-center text-sm space-x-2">
+			<span className="bg-white/20 p-2 rounded-lg shadow-md flex items-center">
+				<span className="text-white">{icon}</span>
+			</span>
+			<h1 className="font-semibold text-white">{label}:</h1>
+			<p className="ml-1 text-white/80">{content}</p>
+		</div>
+	);
 }
