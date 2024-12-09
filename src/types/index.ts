f3a7fe07af42d50f75282a59a73574868301c5ee
scrappie <notscrappie@proton.me>
2024-12-09 16:32:01 +0200

    refactor(types): added tsdocs to every type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,58 +1,66 @@
 export type ResolveVanityResponse = {
+    /** The response object containing the Steam ID, message and success code. */
     response: {
+        /** The resolved SteamID64. */
         steamid: string;
+        /** The success code of the request. */
+        success: number;
+        /** The message returned back from the API (this usually spits back the errors like "Not found"). */
+        message?: string;
     }
 }
 
-export type IGetWishlist = {
+export type UserWishlist = {
+    /** The response object containing an array of items that contains app IDs, their priority & date added timestamp. */
     response: {
+        /** The array of items. */
         items: {
+            /** The app's Steam app ID. */
             appid: number;
         }[];
     }
 }
 
-export type ISteamGame = {
-    fetchGameJson: {
-        [appid: number]: {
-            data: {
-                type: 'game';
+export type SteamGame = {
+    [appid: number]: {
+        success: boolean;
+        data: {
+            type: 'game';
+            name: string;
+            steam_appid: number;
+            is_free: boolean;
+            detailed_description: string;
+            about_the_game: string;
+            short_description: string;
+            header_image: string;
+            capsule_image: string;
+            capsule_imagev5: string;
+            developers: string[];
+            publishers: string[];
+            categories: {
+                id: number;
+                description: string;
+            }[];
+            genres: {
+                id: number;
+                description: string;
+            }[];
+            movies: {
+                id: number;
                 name: string;
-                steam_appid: number;
-                is_free: boolean;
-                detailed_description: string;
-                about_the_game: string;
-                short_description: string;
-                header_image: string;
-                capsule_image: string;
-                capsule_imagev5: string;
-                developers: string[];
-                publishers: string[];
-                categories: {
-                    id: number;
-                    description: string;
-                }[];
-                genres: {
-                    id: number;
-                    description: string;
-                }[];
-                movies: {
-                    id: number;
-                    name: string;
-                    webm?: {
-                        480?: string;
-                        max?: string;
-                    };
-                    mp4?: {
-                        480?: string;
-                        max?: string;
-                    };
-                }[];
-                release_date: {
-                    date: string;
+                webm?: {
+                    480?: string;
+                    max?: string;
                 };
-                background: string;
-            }
+                mp4?: {
+                    480?: string;
+                    max?: string;
+                };
+            }[];
+            release_date: {
+                date: string;
+            };
+            background: string;
         }
     }
 }
