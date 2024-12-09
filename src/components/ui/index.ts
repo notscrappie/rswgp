50c13e4aacb70bb98114d116579862d271829ef4
scrappie <notscrappie@proton.me>
2024-12-09 16:22:53 +0200

    feat(components): added index for easier imports

---
+++ src/components/ui/index.ts
@@ -0,0 +1,45 @@
+import { Button } from './button';
+import {
+	Card,
+	CardHeader,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardTitle,
+} from './card';
+import { Footer } from './footer';
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from './form';
+import { GameCard } from './gameCard';
+import { Header } from './header';
+import { Input } from './input';
+import { Label } from './label';
+
+export {
+	Button,
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+	Footer,
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+	GameCard,
+	Header,
+	Input,
+	Label,
+};
